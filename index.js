9fca2d5f9c5e0674793281634a89e9d45fc81b77
Hải Trình Nguyễn <nguyenmaster3012@gmail.com>
2022-06-05 17:03:56 +0700

    fix event play video javascript and styling for html

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
-// var video = document.getElementById("myVideo");
+var video = document.getElementById("myVideo");
 // var btn = document.getElementById("myBtn");
 
-// function myFunction() {
-//   if (video.paused) {
-//     video.play();
-//     btn.innerHTML = "Pause";
-//   } else {
-//     video.pause();
-//     btn.innerHTML = "Play";
-//   }
-// }
+video.onmousemove = function() {
+    video.play();
+}
+
+/*
+function myFunction() {
+  if (video.paused) {
+    video.play();
+    btn.innerHTML = "Pause";
+  } else {
+    video.pause();
+    btn.innerHTML = "Play";
+  }
+}
+*/
